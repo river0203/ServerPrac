32bc6f74690600b1c52ef95e65dedff39bfa9b3b
river0203 <aspp3666@gmail.com>
2023-08-23 18:36:45 +0900

    mutex 연습

---
+++ ServerExample/ServerExample/ServerExample.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+
+/*int main()
+{
+	std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+
+	for (int j = 0; j < 10; j++)
+	{
+		cout << "first running for <1>" << j << "\n";
+	}
+	std::chrono::duration<double>sec = std::chrono::system_clock::now() - start;
+	std::cout << "for문을 돌리는데 걸리는 시간(초) : " << sec.count() << "seconds" << std::endl;
+
+	return 0;
+}*/
